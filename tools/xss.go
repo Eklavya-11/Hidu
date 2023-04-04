c6ce88d3d6d54e31168f4a755367b1f70a17926e
Eklavya11 <117552685+Eklavya-11@users.noreply.github.com>
2023-04-04 06:25:09 +0200

    Update xss.go

--- tools/xss.go
+++ tools/xss.go
@@ -1,6 +1,32 @@
 // Maybe another repo
 
+package main
+
+import (
+        "bufio"
+        "crypto/tls"
+        "flag"
+        "fmt"
+        "io/ioutil"
+        "net"
+        "net/http"
+        "net/url"
+        "os"
+        "strings"
+        "regexp"
+        "sync"
+        "time"
+)
+
 func main() {
+          
+        var xsspayload string
+        flag.StringVar(&xsspayload, "payload", "", "")
+        flag.StringVar(&xsspayload, "p", "", "")
+
+        var proxy string
+        flag.StringVar(&proxy,"proxy", "0","")
+        flag.StringVar(&proxy,"x", "0","")
           // Headers flag
         flag.Var(&headers, "headers", "")
         flag.Var(&headers, "H", "")
